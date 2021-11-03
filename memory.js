577712077c889cd187cfbb0b18c7816546d895e1
Ludo Tielbeke <97067883@st.deltion.nl>
2021-11-03 16:34:13 +0100

    added resetGame()

--- memory.js
+++ memory.js
@@ -1,9 +1,11 @@
 
+punten = 0;
 userClick =0;
 card1Clicked = 0;
 card2Clicked = 0;
 card1ClickedNummer =0;
 card2ClickedNummer =0;
+document.getElementById("punten").innerHTML = "Punten: " + punten;
 
 function cardSelected(cardId, cardNummer) {
     document.getElementById(cardNummer).value = cardId;
@@ -16,7 +18,8 @@ function cardSelected(cardId, cardNummer) {
         userClick = 0;
         card2Clicked = cardId;
         if (card1Clicked == card2Clicked) {
-            alert("yay");
+            punten++;
+            document.getElementById("punten").innerHTML = "Punten: " + punten;
         }else{
             card1Clicked = 0;
             card2Clicked = 0;
@@ -24,5 +27,20 @@ function cardSelected(cardId, cardNummer) {
             document.getElementById(card2ClickedNummer).value = " ";
         }
     }
-
+    if (punten == 2) {
+        document.getElementById("resetGame").style.display = "block";
+    }
+}
+function resetGame(params) {
+    punten = 0;
+    userClick =0;
+    card1Clicked = 0;
+    card2Clicked = 0;
+    card1ClickedNummer =0;
+    card2ClickedNummer =0;
+    document.getElementById("resetGame").style.display = "none";
+    document.getElementById("punten").innerHTML = "Punten: " + punten;
+    for (let i = 1; i <= 4; i++) {
+        document.getElementById("kaart" + i).value = " ";
+    }
 }
